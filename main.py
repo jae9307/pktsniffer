9921e6eab82373961bfc12c1fb0e179b60486f44
JoseEstevez <jae9307@g.rit.edu>
2025-02-04 18:35:53 -0500

    Refactored codes into separate functions

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from scapy.all import *
 
-a = rdpcap("test.pcap")
+def udp():
+    encapsulated_layer = packet.getlayer("UDP")
 
-index = 1
-for packet in a:
-    ip_version = 4
+    if encapsulated_layer is not None:
+        print("UDP LAYER")
+        print(f"SRC Port: {encapsulated_layer.sport}")
+        print(f"Checksum: {encapsulated_layer.chksum}")
 
+def tcp():
+    encapsulated_layer = packet.getlayer("TCP")
+    if encapsulated_layer is not None:
+        print("TCP LAYER")
+        print(f"Flags: {encapsulated_layer.flags}")
+        print(f"Window: {encapsulated_layer.window}")
+        print(f"Acknowledgement Number: {encapsulated_layer.ack}")
+    else:
+        udp()
+
+def ethernet():
     ethernet_layer = packet.getlayer("Ether")
-    ip_layer = packet.getlayer("IP")
-    if ip_layer is None:
-        ip_layer = packet.getlayer("IPv6")
-        ip_version = 6
 
-    print(f"---------    Packet {index}    ---------")
     print("ETHERNET HEADER")
     print(f"MAC DST: {ethernet_layer.dst}")
     print(f"MAC SRC: {ethernet_layer.src}")
     print(f"Ethertype: {ethernet_layer.type}")
+
+def ip():
+    ip_version = 4
+    ip_layer = packet.getlayer("IP")
+    if ip_layer is None:
+        ip_layer = packet.getlayer("IPv6")
+        ip_version = 6
 
     if ip_layer is not None:
         print(f"Packet Size: {ip_layer.len}")
@@ -37,18 +52,14 @@ for packet in a:
             print(f"Time to Live: {ip_layer.ttl}")
             print(f"Protocol: {ip_layer.proto}")
 
-    encapsulated_layer = packet.getlayer("TCP")
-    if encapsulated_layer is not None:
-        print("TCP LAYER")
-        print(f"Flags: {encapsulated_layer.flags}")
-        print(f"Window: {encapsulated_layer.window}")
-        print(f"Acknowledgement Number: {encapsulated_layer.ack}")
-    else:
-        encapsulated_layer = packet.getlayer("UDP")
+a = rdpcap("test.pcap")
 
-        if encapsulated_layer is not None:
-            print("UDP LAYER")
-            print(f"SRC Port: {encapsulated_layer.sport}")
-            print(f"Checksum: {encapsulated_layer.chksum}")
+index = 1
+for packet in a:
+    print(f"---------    Packet {index}    ---------")
+
+    ethernet()
+    ip()
+    tcp()
 
     index += 1
