c050ad4ee8a213754f67c17f786427378c52cb6e
JoseEstevez <jae9307@g.rit.edu>
2025-02-05 00:00:12 -0500

    Refactored code to parse command line arguments and filter accordingly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 
-def udp():
+def read_udp_layer():
     encapsulated_layer = packet.getlayer("UDP")
 
     if encapsulated_layer is not None:
@@ -8,7 +8,7 @@ def udp():
         print(f"SRC Port: {encapsulated_layer.sport}")
         print(f"Checksum: {encapsulated_layer.chksum}")
 
-def tcp():
+def read_tcp_layer():
     encapsulated_layer = packet.getlayer("TCP")
     if encapsulated_layer is not None:
         print("TCP LAYER")
@@ -16,9 +16,9 @@ def tcp():
         print(f"Window: {encapsulated_layer.window}")
         print(f"Acknowledgement Number: {encapsulated_layer.ack}")
     else:
-        udp()
+        read_udp_layer()
 
-def ethernet():
+def read_ethernet_layer():
     ethernet_layer = packet.getlayer("Ether")
 
     print("ETHERNET HEADER")
@@ -26,7 +26,7 @@ def ethernet():
     print(f"MAC SRC: {ethernet_layer.src}")
     print(f"Ethertype: {ethernet_layer.type}")
 
-def ip():
+def read_ip_layer():
     ip_version = 4
     ip_layer = packet.getlayer("IP")
     if ip_layer is None:
@@ -52,14 +52,56 @@ def ip():
             print(f"Time to Live: {ip_layer.ttl}")
             print(f"Protocol: {ip_layer.proto}")
 
-a = rdpcap("test.pcap")
+def filtering(args, packet):
+    ip_layer = packet.getlayer("IP")
+    if ip_layer is None:
+        ip_layer = packet.getlayer("IPv6")
+
+    if ip_layer is not None:
+        if args.host is not None and ip_layer.src != args.host and ip_layer.dst != args.host:
+            return False
+    elif args.ip is True:
+            return False
+
+    tcp_layer = packet.getlayer("TCP")
+    if tcp_layer is not None:
+        if args.port is not None and tcp_layer.sport != args.port and tcp_layer.dport != args.port:
+            return False
+    elif args.tcp is True:
+        return False
+
+    udp_layer = packet.getlayer("UDP")
+    if udp_layer is None and args.udp is True:
+        return False
+
+    icmp_layer = packet.getlayer("ICMP")
+    if icmp_layer is None and args.icmp is True:
+        return False
+
+    return True
+
+parser = argparse.ArgumentParser(prog='pktsniffer', description='Reads packet headers')
+parser.add_argument('filename')
+parser.add_argument('-host', action='store')
+parser.add_argument('-port', action='store')
+parser.add_argument('-ip', action='store_true')
+parser.add_argument('-tcp', action='store_true')
+parser.add_argument('-udp', action='store_true')
+parser.add_argument('-icmp', action='store_true')
+parser.add_argument('-net', action='store')
+parser.add_argument('-c', action='store')
+
+args = parser.parse_args()
+
+packets = rdpcap(args.filename)
 
 index = 1
-for packet in a:
-    print(f"---------    Packet {index}    ---------")
+for packet in packets:
+    if filtering(args, packet):
+        print(f"---------    Packet {index}    ---------")
 
-    ethernet()
-    ip()
-    tcp()
+        read_ethernet_layer()
+        read_ip_layer()
+        read_tcp_layer()
 
-    index += 1
+        index += 1
